c7e9e680a6ecb94ba0436b8d93ef32fc59b9ada6
LAPTOP-HCMQF8S9\Agustin <pepe_diaz92@hotmail.com>
2017-04-17 22:47:30 -0500

    weeeee

---
+++ Assets/camerafollow.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerafollow : MonoBehaviour {
+
+	private const float Y_ANGLE_MIN = -10.0f;
+	private const float Y_ANGLE_MAX = 50.0f;
+	public Transform target;
+	public Transform camTransform;
+
+	private Camera cam;
+
+	private float distance =2.0f;
+	private float currentX = 0.0f;
+	private float currentY = 0.0f;
+	private float sensivityX = 4.0f;
+	private float sensivityY = 1.0f;
+
+	private Vector3 finalpos;
+	private void Start(){
+		camTransform = transform;
+		cam = Camera.main;
+		finalpos = new Vector3 (-9.9f, 1.37f, -39.32f);
+		transform.position = finalpos;
+	}
+	private void Update(){
+		currentX += Input.GetAxis ("Mouse Y");
+		currentY += Input.GetAxis ("Mouse X");
+
+		currentX = Mathf.Clamp (currentX, Y_ANGLE_MIN, Y_ANGLE_MAX);
+
+		if(Input.GetAxis("Mouse ScrollWheel")< 0){
+			Camera.main.fieldOfView++;
+		}
+		if(Input.GetAxis("Mouse ScrollWheel")> 0){
+			Camera.main.fieldOfView--;
+		}
+	}
+
+	private void LateUpdate(){
+		Vector3 dir = new Vector3 (0, 0, -distance);
+		Quaternion rotation = Quaternion.Euler (currentX, currentY, 0);
+		camTransform.position = target.position + rotation * dir;
+		camTransform.LookAt (target.position);
+
+	}
+}
---
+++ Assets/fullturn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fullturn : MonoBehaviour {
+
+
+	private bool turn;
+	private float timetodothisloop;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	void Awake(){
+		turn = true;
+	}
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("w") && turn == true){ //rotate script here 
+			turn = false; 
+			for (var i = 0; i < timetodothisloop; i++){
+
+				transform.Rotate(0,180/timetodothisloop,0);
+			}
+			turn = true;
+		}
+	}
+}
